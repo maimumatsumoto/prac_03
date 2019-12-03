1caadfffaee2aa7b81fa7853198a0cbaac7b8715
maimumatsumoto <maimu.matsumoto@my.jcu.edu.au>
2019-12-03 23:19:47 +0800

    created 3 different function. The first one is to print the menu, second one is to convert fahrenheit to celsius and the third one is to convert celsius to fahrenheit.

--- temperatures.py
+++ temperatures.py
@@ -1,24 +1,38 @@
-"""
-CP1404/CP5632 - Practical
-Pseudocode for temperature conversion
-"""
 
-MENU = """C - Convert Celsius to Fahrenheit
-F - Convert Fahrenheit to Celsius
-Q - Quit"""
-print(MENU)
-choice = input(">>> ").upper()
-while choice != "Q":
-    if choice == "C":
-        celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
-        print("Result: {:.2f} F".format(fahrenheit))
-    elif choice == "F":
-        fahrenheit = float(input("Fahrenheit: "))
-        celsius = 5 / 9 * (fahrenheit - 32)
-        print("Result: {:.2f} C".format(celsius))
-    else:
-        print("Invalid option")
-    print(MENU)
+def main():
+    print_menu()
     choice = input(">>> ").upper()
-print("Thank you.")
+    while choice != "Q":
+        if choice == "C":
+            cel_to_fahr()
+        elif choice == "F":
+            fahr_to_cel()
+        else:
+            print("Invalid option")
+        print_menu()
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+
+def print_menu():
+    MENU = """C - Convert Celsius to Fahrenheit
+    F - Convert Fahrenheit to Celsius
+    Q - Quit"""
+    print(MENU)
+    
+
+
+def fahr_to_cel():
+    fahrenheit = float(input("Fahrenheit: "))
+    celsius = 5 / 9 * (fahrenheit - 32)
+    print("Result: {:.2f} C".format(celsius))
+
+
+def cel_to_fahr():
+    celsius = float(input("Celsius: "))
+    fahrenheit = celsius * 9.0 / 5 + 32
+    print("Result: {:.2f} F".format(fahrenheit))
+
+
+
+main()
